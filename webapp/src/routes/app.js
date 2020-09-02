1aba76960b15494ac67953a04231e2a86135751c
Alexandra Capatina <capatinaalexandra@gmail.com>
2020-09-02 18:21:02 +0300

    remove private route

--- webapp/src/routes/app.js
+++ webapp/src/routes/app.js
@@ -2,8 +2,6 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-import PrivateRoute from '../components/routing/PrivateRoute';
-
 import Dashboard from 'features/dashboard/Dashboard'
 import Settings from 'features/settings/Settings'
 import NotFound from 'components/common/NotFound';
@@ -11,8 +9,8 @@ import Forbidden from 'components/common/Forbidden';
 
 export default (
     <Switch>
-        <PrivateRoute exact path="/dashboard" component={Dashboard}/>
-        <PrivateRoute exact path="/settings" component={Settings}/>
+        <Route exact path="/dashboard" component={Dashboard}/>
+        <Route exact path="/settings" component={Settings}/>
         <Redirect exact from="/" to="/dashboard" />
         <Route exact path="/forbidden" component={Forbidden} />
         <Route render={() => <NotFound title="PageNotFound"></NotFound>} />
