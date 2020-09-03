ce01e63f4744ad0dd01b3d4de74c42811e2f4044
Alexandra Capatina <capatinaalexandra@gmail.com>
2020-09-03 17:23:57 +0300

    remove identity check
    (cherry picked from commit 0bc44c29776bb9ec4aab0b426bec8640da9e3d7f)

---
+++ webapp/src/features/conference/edit/components/ConferenceContainer.js
@@ -0,0 +1,14 @@
+
+import React, { Fragment } from 'react';
+import { Typography, Grid } from '@material-ui/core';
+import Conference from './Conference'
+
+function ConferenceContainer() {
+    return (
+        <Conference>
+
+        </Conference>
+    );
+}
+
+export default ConferenceContainer;
