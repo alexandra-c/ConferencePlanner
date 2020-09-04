e2629e7e4db77b91a71b5bf14c6b5185513a0ffa
Alexandra Capatina <capatinaalexandra@gmail.com>
2020-09-04 13:50:34 +0300

    hello world

--- graphql-server/src/startup/schema.js
+++ graphql-server/src/startup/schema.js
@@ -7,9 +7,11 @@ const paginationTypeDefs = require('../features/common/paginationSchema');
 const userTypeDefs = require('../features/user/schema');
 const userResolvers = require('../features/user/resolvers');
 
+const helloWorldTypeDefs = require('../features/helloWorld/schema');
+const helloWorldResolvers = require('../features/helloWorld/resolvers');
 
-const typeDefs = [rootTypeDefs, paginationTypeDefs, userTypeDefs]
-const resolvers = merge(userResolvers)
+const typeDefs = [rootTypeDefs, paginationTypeDefs, userTypeDefs, helloWorldTypeDefs]
+const resolvers = merge(userResolvers, helloWorldResolvers)
 
 module.exports = makeExecutableSchema({ typeDefs, resolvers });
 module.exports.tests = { typeDefs, resolvers }
