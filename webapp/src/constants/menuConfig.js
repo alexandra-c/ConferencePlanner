d20946c0d2170a12ac63cf716771c943cb0400c1
Costi <cdiaconita@totalsoft.ro>
2020-09-04 17:00:54 +0300

    my conference list screen

--- webapp/src/constants/menuConfig.js
+++ webapp/src/constants/menuConfig.js
@@ -6,7 +6,7 @@ import EventNoteIcon from '@material-ui/icons/EventNote';
 
 const menuItems = [
     { icon: <EventIcon />, text: 'NavBar.Conferences', path: '/conferences', name: 'Conferences' },
-    { icon: <EventNoteIcon />, text: 'NavBar.MyConferences', path: '/myconferences', name: 'My Conferences' },
+    { icon: <EventNoteIcon />, text: 'NavBar.MyConferences', path: '/myconferences', name: 'MyConferences' },
     { icon: <Dashboard />, text: 'NavBar.Dashboard', path: '/dashboard', name: 'Dashboard' },
     { icon: <Settings />, text: 'NavBar.Settings', path: '/settings', name: 'Settings' }
 ]
--- webapp/src/routes/app.js
+++ webapp/src/routes/app.js
@@ -3,16 +3,16 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Dashboard from 'features/dashboard/Dashboard'
-import Settings from 'features/settings/Settings'
 import NotFound from 'components/common/NotFound';
 import Forbidden from 'components/common/Forbidden';
 import ConferenceListContainer from 'features/conference/list/components/ConferenceListContainer';
+import MyConferenceListContainer from 'features/myConference/list/components/MyConferenceListContainer';
 
 export default (
     <Switch>
         <Route exact path="/dashboard" component={Dashboard} />
         <Route exact path="/conferences" component={ConferenceListContainer} />
-        <Route exact path="/myconferences" component={Settings} />
+        <Route exact path="/myconferences" component={MyConferenceListContainer} />
         <Redirect exact from="/" to="/dashboard" />
         <Route exact path="/forbidden" component={Forbidden} />
         <Route render={() => <NotFound title="PageNotFound"></NotFound>} />
