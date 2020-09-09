7a11fb6997bc0f12378d32613e0c33c6d0239099
Alexandra <capatinaalexandra@gmail.com>
2020-09-09 11:17:24 +0300

    hello world

--- webapp/src/constants/menuConfig.js
+++ webapp/src/constants/menuConfig.js
@@ -5,7 +5,7 @@ import EventNoteIcon from '@material-ui/icons/EventNote';
 
 const menuItems = [
     { icon: <HomeIcon />, text: 'NavBar.Welcome', path: '/welcome', name: 'Welcome' },
-    // { icon: <HomeIcon />, text: 'NavBar.MyFirstMenu', path: '/helloWorld', name: 'MyFirstMenu' },
+    { icon: <HomeIcon />, text: 'NavBar.MyFirstMenu', path: '/helloWorld', name: 'MyFirstMenu' },
     { icon: <EventIcon />, text: 'NavBar.Conferences', path: '/conferences', name: 'Conferences' },
     { icon: <EventNoteIcon />, text: 'NavBar.MyConferences', path: '/myconferences', name: 'My Conferences' }
 ]
