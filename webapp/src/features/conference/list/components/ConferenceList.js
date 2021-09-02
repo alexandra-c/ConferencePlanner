d0d30ef43b0a8c851626000514934b678f27b0ac
Elena Dumitrescu <edumitrescu@totalsoftostan.onmicrosoft.com>
2021-09-02 10:56:20 +0300

    test all works

--- webapp/src/features/conference/list/components/ConferenceList.js
+++ webapp/src/features/conference/list/components/ConferenceList.js
@@ -1,29 +1,24 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Grid } from '@material-ui/core';
-import ConferenceItem from "./ConferenceItem";
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Grid } from '@material-ui/core'
+import ConferenceItem from './ConferenceItem'
 
 const ConferenceList = ({ conferences, onAttend, onWithdraw }) => {
-
-    return (
-        <Grid container spacing={2}>
-            {conferences?.map(conference =>
-                <Grid item xs={12} lg={4} key={conference.id}>
-                    <ConferenceItem
-                        conference={conference}
-                        onAttend={onAttend}
-                        onWithdraw={onWithdraw}
-                    />
-                </Grid>
-            )}
+  return (
+    <Grid container spacing={2}>
+      {conferences?.map(conference => (
+        <Grid item xs={12} lg={4} key={conference.id}>
+          <ConferenceItem conference={conference} onAttend={onAttend} onWithdraw={onWithdraw} />
         </Grid>
-    )
+      ))}
+    </Grid>
+  )
 }
 
 ConferenceList.propTypes = {
-    conferences: PropTypes.array,
-    onAttend: PropTypes.func.isRequired,
-    onWithdraw: PropTypes.func.isRequired
+  conferences: PropTypes.array,
+  onAttend: PropTypes.func.isRequired,
+  onWithdraw: PropTypes.func.isRequired
 }
 
-export default ConferenceList;
+export default ConferenceList
