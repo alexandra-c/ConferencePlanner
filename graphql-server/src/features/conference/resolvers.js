298c4c1aa52fa3258b8d59e5ab191252f7ffd960
Costinel Diaconita <cdiaconita@totalsoft.ro>
2021-09-19 22:01:39 +0300

    aaded api integration

--- graphql-server/src/features/conference/resolvers.js
+++ graphql-server/src/features/conference/resolvers.js
@@ -1,95 +1,99 @@
-const { randomCharacters } = require("../../utils/functions");
+const { randomCharacters } = require('../../utils/functions')
 
 const conferenceResolvers = {
-    Query: {
-        conferenceList: async (_parent, { pager, filters }, { dataSources }, _info) => {
-            const data = await dataSources.conferenceDb.getConferenceList(pager, filters);
-            return data
-        },
-        conference: async (_parent, { id }, { dataLoaders }, _info) => {
-            const result = await dataLoaders.conferenceById.load(id);
-            return result;
-        }
+  Query: {
+    conferenceList: async (_parent, { pager, filters }, { dataSources }, _info) => {
+      const data = await dataSources.conferenceDb.getConferenceList(pager, filters)
+      return data
     },
-    ConferenceList: {
-        pagination: async (_parent, { pager, filters }, { dataSources }, _info) => {
-            const { totalCount } = await dataSources.conferenceDb.getConferenceListTotalCount(filters);
-            return { currentPage: pager, totalCount };
-        }
-    },
-    Conference: {
-        speakers: async ({ id }, _arguments, { dataLoaders }, _info) => {
-            const speakers = await dataLoaders.speakersByConferenceId.load(id);
-            return speakers;
-        },
-        type: async ({ conferenceTypeId }, _params, { dataLoaders }, _info) => {
-            const conferenceType = await dataLoaders.conferenceTypeById.load(conferenceTypeId);
-            return conferenceType;
-        },
-        category: async ({ categoryId }, _params, { dataLoaders }, _info) => {
-            const category = await dataLoaders.categoryById.load(categoryId);
-            return category;
-        },
-        status: async ({ id }, { userEmail }, { dataLoaders }, _info) => {
-            const status = await dataLoaders.statusByConferenceId.load({ id, userEmail })
-            return status
-        },
-        location: async ({ locationId }, _params, { dataLoaders }, _info) => {
-            const location = await dataLoaders.locationById.load(locationId);
-            return location;
-        }
-    },
-    Location: {
-        city: async ({ cityId }, _params, { dataLoaders }, _info) => {
-            const city = await dataLoaders.cityById.load(cityId);
-            return city;
-        },
-        county: async ({ countyId }, _params, { dataLoaders }, _info) => {
-            const county = await dataLoaders.countyById.load(countyId);
-            return county;
-        },
-        country: async ({ countryId }, _params, { dataLoaders }) => {
-            const country = await dataLoaders.countryById.load(countryId);
-            return country;
-        }
-    },
-    Mutation: {
-        attend: async (_parent, { input }, { dataSources }, _info) => {
-            const updateInput = { ...input, statusId: 3 /* Attended */ }
-            const statusId = await dataSources.conferenceDb.updateConferenceXAttendee(updateInput);
-            return statusId ? randomCharacters(10) : null
-        },
-        withdraw: async (_parent, { input }, { dataSources }, _info) => {
-            const updateInput = { ...input, statusId: 2 /* Withdrawn */ }
-            const statusId = await dataSources.conferenceDb.updateConferenceXAttendee(updateInput);
-            return statusId
-        },
-        saveConference: async (_parent, { input }, { dataSources }, _info) => {
-            const location = await dataSources.conferenceDb.updateLocation(input.location);
-
-            const updatedConference = await dataSources.conferenceDb.updateConference({ ...input, location })
-
-            const speakers = await Promise.all(input.speakers.map(async speaker => {
-                const updatedSpeaker = await dataSources.conferenceDb.updateSpeaker(speaker);
-                const isMainSpeaker = await dataSources.conferenceDb.updateConferenceXSpeaker(
-                    {
-                        speakerId: updatedSpeaker.id,
-                        isMainSpeaker: speaker.isMainSpeaker,
-                        conferenceId: updatedConference.id
-                    }
-                );
-                return { ...updatedSpeaker, isMainSpeaker }
-            }))
-
-            input.deletedSpeakers && input.deletedSpeakers.length > 0 && await dataSources.conferenceDb.deleteSpeaker(input.deletedSpeakers)
-
-            return { ...updatedConference, location, speakers }
-        },
-        deleteConference: async (_parent, { id }, { dataSources }, _info) => {
-            const name = await dataSources.conferenceDb.deleteConference(id);
-            return name
-        }
+    conference: async (_parent, { id }, { dataLoaders }, _info) => {
+      const result = await dataLoaders.conferenceById.load(id)
+      return result
     }
-};
+  },
+  ConferenceList: {
+    pagination: async (_parent, { pager, filters }, { dataSources }, _info) => {
+      const { totalCount } = await dataSources.conferenceDb.getConferenceListTotalCount(filters)
+      return { currentPage: pager, totalCount }
+    }
+  },
+  Conference: {
+    speakers: async ({ id }, _arguments, { dataLoaders }, _info) => {
+      const speakers = await dataLoaders.speakersByConferenceId.load(id)
+      return speakers
+    },
+    type: async ({ conferenceTypeId }, _params, { dataLoaders }, _info) => {
+      const conferenceType = await dataLoaders.conferenceTypeById.load(conferenceTypeId)
+      return conferenceType
+    },
+    category: async ({ categoryId }, _params, { dataLoaders }, _info) => {
+      const category = await dataLoaders.categoryById.load(categoryId)
+      return category
+    },
+    status: async ({ id }, { userEmail }, { dataLoaders }, _info) => {
+      const status = await dataLoaders.statusByConferenceId.load({ id, userEmail })
+      return status
+    },
+    location: async ({ locationId }, _params, { dataLoaders }, _info) => {
+      const location = await dataLoaders.locationById.load(locationId)
+      return location
+    }
+  },
+  Location: {
+    city: async ({ cityId }, _params, { dataLoaders }, _info) => {
+      const city = await dataLoaders.cityById.load(cityId)
+      return city
+    },
+    county: async ({ countyId }, _params, { dataLoaders }, _info) => {
+      const county = await dataLoaders.countyById.load(countyId)
+      return county
+    },
+    country: async ({ countryId }, _params, { dataLoaders }) => {
+      const country = await dataLoaders.countryById.load(countryId)
+      return country
+    }
+  },
+  Mutation: {
+    attend: async (_parent, { input }, { dataSources }, _info) => {
+      const updateInput = { ...input, statusId: 3 /* Attended */ }
+      const suggestedConferences = await dataSources.conferenceApi.getConferenceSuggestions(input)
+      const statusId = await dataSources.conferenceDb.updateConferenceXAttendee(updateInput)
+      // const code = statusId ? randomCharacters(10) : null
+      return statusId ? randomCharacters(10) : null
+    },
+    withdraw: async (_parent, { input }, { dataSources }, _info) => {
+      const updateInput = { ...input, statusId: 2 /* Withdrawn */ }
+      const statusId = await dataSources.conferenceDb.updateConferenceXAttendee(updateInput)
+      return statusId
+    },
+    saveConference: async (_parent, { input }, { dataSources }, _info) => {
+      const location = await dataSources.conferenceDb.updateLocation(input.location)
 
-module.exports = conferenceResolvers;
+      const updatedConference = await dataSources.conferenceDb.updateConference({ ...input, location })
+
+      const speakers = await Promise.all(
+        input.speakers.map(async speaker => {
+          const updatedSpeaker = await dataSources.conferenceDb.updateSpeaker(speaker)
+          const isMainSpeaker = await dataSources.conferenceDb.updateConferenceXSpeaker({
+            speakerId: updatedSpeaker.id,
+            isMainSpeaker: speaker.isMainSpeaker,
+            conferenceId: updatedConference.id
+          })
+          return { ...updatedSpeaker, isMainSpeaker }
+        })
+      )
+
+      input.deletedSpeakers &&
+        input.deletedSpeakers.length > 0 &&
+        (await dataSources.conferenceDb.deleteSpeaker(input.deletedSpeakers))
+
+      return { ...updatedConference, location, speakers }
+    },
+    deleteConference: async (_parent, { id }, { dataSources }, _info) => {
+      const name = await dataSources.conferenceDb.deleteConference(id)
+      return name
+    }
+  }
+}
+
+module.exports = conferenceResolvers
--- graphql-server/src/startup/dataSources.js
+++ graphql-server/src/startup/dataSources.js
@@ -1,22 +1,25 @@
-const UserApi = require('../features/user/dataSources/userApi');
-const UserDb = require('../features/user/dataSources/userDb');
-const ConferenceDb = require('../features/conference/conferenceDb');
-const DictionariesDb = require('../features/dictionaries/dictionariesDb');
+const UserApi = require('../features/user/dataSources/userApi')
+const UserDb = require('../features/user/dataSources/userDb')
+const ConferenceDb = require('../features/conference/conferenceDb')
+const DictionariesDb = require('../features/dictionaries/dictionariesDb')
+const ConferenceApi = require('../features/conference/conferenceApi')
 
 const ds = {
-    userApi: new UserApi(),
-    userDb: new UserDb(),
-    conferenceDb: new ConferenceDb(),
-    dictionariesDb: new DictionariesDb()
+  userApi: new UserApi(),
+  userDb: new UserDb(),
+  conferenceDb: new ConferenceDb(),
+  dictionariesDb: new DictionariesDb(),
+  conferenceApi: new ConferenceApi()
 }
 
-module.exports.getDataSources = () => (ds)
+module.exports.getDataSources = () => ds
 
 // This is a temporary fix to pass dataSources to ws requests. This will be fixed in Apollo server v3.0
 module.exports.initializedDataSources = (context, dbInstance) => {
-    ds.userApi.initialize({ context })
-    ds.userDb.initialize({ context: { dbInstance } })
-    ds.conferenceDb.initialize({ context: { dbInstance } })
-    ds.dictionariesDb.initialize({ context: { dbInstance } })
-    return ds
+  ds.userApi.initialize({ context })
+  ds.userDb.initialize({ context: { dbInstance } })
+  ds.conferenceDb.initialize({ context: { dbInstance } })
+  ds.dictionariesDb.initialize({ context: { dbInstance } })
+  ds.conferenceApi.initialize({ context })
+  return ds
 }
