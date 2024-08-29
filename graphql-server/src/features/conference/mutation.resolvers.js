d7bbaa0933a15c02b79cad579710251c3b35b1d3
Andra <andarbuz@gmail.com>
2024-08-29 18:18:25 +0300

    added delete conference

--- graphql-server/src/features/conference/mutation.resolvers.js
+++ graphql-server/src/features/conference/mutation.resolvers.js
@@ -1,10 +1,9 @@
 const { prisma } = require('../../prisma')
 const { map } = require('ramda')
-const { randomCharacters } = require('../../utils/functions')
 
 const conferenceMutationResolvers = {
   Mutation: {
-    saveConference: async (_parent, { input }, _ctx, _info) => {
+    saveConference: async (_parent, { input }, { dataSources }, _info) => {
       const result = await prisma().$transaction(async prismaClient => {
         //we deconstruct the input object so we can use its nested properties in the following sections
         const { id, type, category, location, speakers, deletedSpeakers, ...conferenceInput } = input
@@ -35,7 +34,8 @@ const conferenceMutationResolvers = {
         const updatedConference = await prismaClient.conference.upsert({
           where: { id: id || -1 },
           update: conference,
-          create: conference
+          create: conference,
+          include: { location: true }
         })
 
         //disconnect the deleted speakers from the conference
@@ -62,21 +62,49 @@ const conferenceMutationResolvers = {
         return updatedConference
       })
 
+      // await Promise.all(
+      //   map(async speaker => {
+      //     await dataSources.conferenceApi.sendSMSNotification({
+      //       conferenceId: result.id,
+      //       receiverId: speaker.id
+      //     })
+      //   }, input.speakers)
+      // )
       return result
     },
-    changeAttendanceStatus: async (_parent, { input }, _ctx, _info) => {
-      await prisma().conferenceXAttendee.upsert({
-        where: {
-          attendeeEmail_conferenceId: { conferenceId: input.conferenceId, attendeeEmail: input.attendeeEmail }
-        },
-        update: { statusId: input.statusId },
-        create: {
-          conferenceId: input.conferenceId,
-          attendeeEmail: input.attendeeEmail,
-          statusId: input.statusId
-        }
+    deleteConference: async (_parent, { id }, _ctx, _info) => {
+      const result = await prisma().$transaction(async prismaClient => {
+        await prismaClient.conferenceXAttendee.deleteMany({ where: { conferenceId: id } })
+        await prismaClient.feedback.deleteMany({ where: { conferenceId: id } })
+
+        const conferenceXSpeakers = await prismaClient.conferenceXSpeaker.findMany({
+          where: { conferenceId: id },
+          select: { speakerId: true }
+        })
+
+        // Extract speaker IDs
+        const speakerIds = conferenceXSpeakers.map(cxs => cxs.speakerId)
+
+        // Optionally delete the speakers if they are not associated with other conferences
+        await prismaClient.speaker.deleteMany({
+          where: {
+            id: { in: speakerIds },
+            conferenceXSpeaker: { none: {} } // Check if the speaker is not related to any other conferences
+          }
+        })
+
+        await prismaClient.conferenceXSpeaker.deleteMany({ where: { conferenceId: id } })
+
+        const conference = await prismaClient.conference.findUnique({
+          where: { id },
+          select: { name: true, locationId: true }
+        })
+        await prismaClient.conference.delete({ where: { id } })
+        if (conference?.locationId) await prismaClient.location.delete({ where: { id: conference.locationId } })
+
+        return conference.name
       })
-      return null
+      return result
     }
   }
 }
